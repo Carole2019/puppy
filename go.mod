213dcc268573de6bffd3310c5ec32c2c2db0a59c
unknown <belle4038@gmail.com>
2023-10-04 16:47:43 -0700

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Carole2019/puppy
 
 go 1.21.1
+
+require github.com/Carole2019/dog v0.0.0-20231002230042-9e216d70afe7 // indirect
--- puppy.go
+++ puppy.go
@@ -1,5 +1,9 @@
 package puppy
 
+import (
+	"github.com/Carole2019/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
@@ -7,3 +11,9 @@ func Bark() string {
 func Barks() string {
 	return "Woof! Woof! Woof!"
 }
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
